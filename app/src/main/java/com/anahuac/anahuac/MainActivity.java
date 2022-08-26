f1f4ef1ed3c4bf5f17507bbc8c36bfaf1d8ba2cb
kevsarjona <kevinlovelp@gmail.com>
2022-08-25 21:36:45 -0500

    Logins

--- app/src/main/java/com/anahuac/anahuac/MainActivity.java
+++ app/src/main/java/com/anahuac/anahuac/MainActivity.java
@@ -12,6 +12,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity  {
 
     private Button btn_login_fb;
+    private Button btn_login_net;
 
 
     @Override
@@ -20,6 +21,7 @@ public class MainActivity extends AppCompatActivity  {
         setContentView(R.layout.activity_main);
 
         btn_login_fb = findViewById(R.id.btn_login_fb);
+        btn_login_net = findViewById(R.id.btn_login_netflix);
 
         btn_login_fb.setOnClickListener(new View.OnClickListener() {
 
@@ -27,6 +29,16 @@ public class MainActivity extends AppCompatActivity  {
             public void onClick(View view){
                 Toast.makeText( MainActivity.this, "Boton pulsado", Toast.LENGTH_LONG).show();
                 Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_login_net.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view){
+                Toast.makeText( MainActivity.this, "Boton pulsado", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(MainActivity.this, LoginNetflixActivity.class);
                 startActivity(intent);
             }
         });
