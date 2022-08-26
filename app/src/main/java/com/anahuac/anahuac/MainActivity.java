e77cab0293828d51879f23eed35257d04b21b756
kevsarjona <kevinlovelp@gmail.com>
2022-08-25 19:44:50 -0500

    Primer commit

---
+++ app/src/main/java/com/anahuac/anahuac/MainActivity.java
@@ -0,0 +1,39 @@
+package com.anahuac.anahuac;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity  {
+
+    private Button btn_login_fb;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_login_fb = findViewById(R.id.btn_login_fb);
+
+        btn_login_fb.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view){
+                Toast.makeText( MainActivity.this, "Boton pulsado", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+    }
+
+
+}
