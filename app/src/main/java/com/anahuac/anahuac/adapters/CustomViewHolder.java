2fea1a7d0ac91760dcb85d4bab0dca9c04d570cc
kevsarjona <kevinlovelp@gmail.com>
2022-09-22 23:10:50 -0500

    Primer parcial

---
+++ app/src/main/java/com/anahuac/anahuac/adapters/CustomViewHolder.java
@@ -0,0 +1,12 @@
+package com.anahuac.anahuac.adapters;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CustomViewHolder extends RecyclerView.ViewHolder {
+    public CustomViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
